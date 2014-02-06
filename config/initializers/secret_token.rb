36bf85bdeeb6eb9818d0f8b83acadecc098a181d
Toly <tolyshilman@yahoo.com>
2014-02-06 12:47:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitpins::Application.config.secret_key_base = '1c1e8fa1525eb76203a96b8daa0eeada6bd87bc051d88eb9ac2e1745f53f16ea8ee3528b5778856d74e0d38f7947a424ee0b2aabe62026ba9a5dd8c092e3bd7d'
